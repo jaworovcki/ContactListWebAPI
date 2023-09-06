8854f294f627ef5dbc1d8a6db851ec1b51fb7779
Andrii <jaworovcki@gmail.com>
2023-09-06 17:15:46 +0300

    init: Person model init: DbContext class

---
+++ ContactListWebAPI/DataAccess/ContactDataContext.cs
@@ -0,0 +1,14 @@
+﻿using ContactListWebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactListWebAPI.DataAccess
+{
+    public class ContactDataContext : DbContext
+    {
+        public DbSet<Person> People { get; set; } = null!;
+        public ContactDataContext(DbContextOptions<ContactDataContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ ContactListWebAPI/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace ContactListWebAPI.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+    }
+}
