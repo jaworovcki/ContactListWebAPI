bb3300d748d81667df3e5e8acdc37ba72d6aa811
Andrii <jaworovcki@gmail.com>
2023-09-06 17:32:24 +0300

    init: ContactsController

---
+++ ContactListWebAPI/Controllers/ContactsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactListWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+
+    }
+}
